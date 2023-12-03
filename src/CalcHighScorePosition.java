c79bd5ef0393c4e042eaaab1f1d6be4b0bbe7fc8
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-03 21:52:24 +0530

    calculate high score of player using if else-if else condition

---
+++ src/CalcHighScorePosition.java
@@ -0,0 +1,27 @@
+public class CalcHighScorePosition {
+    public static void main(String[] args) {
+//    1500,1000,500,100,25
+        displayHighScorePosition("PS Solanki", calculateHighScorePosition(1500));
+        displayHighScorePosition("Sam", calculateHighScorePosition(1000));
+        displayHighScorePosition("Altman", calculateHighScorePosition(500));
+        displayHighScorePosition("Rocky", calculateHighScorePosition(100));
+        displayHighScorePosition("Superman", calculateHighScorePosition(25));
+    }
+
+    public static void displayHighScorePosition(String playerName, int playerPos) {
+        System.out.println(playerName + " managed to get into position " + playerPos + " on the high score list");
+    }
+
+    //    learn how to create if else-if else condition
+    public static int calculateHighScorePosition(int playerScore) {
+        int position = 4;
+        if (playerScore >= 1000)
+            position = 1;
+        else if (playerScore >= 500)
+            position = 2;
+        else if (playerScore >= 100)
+            position = 3;
+
+        return position;
+    }
+}
