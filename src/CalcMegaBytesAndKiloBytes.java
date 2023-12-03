943d4a689a62ccc8a21283e2dacb533ed0734031
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-03 22:05:24 +0530

    calculate MB and KB from kiloBytes i.e. if input is kB = 5000  => 4 MB and 904 KB

---
+++ src/CalcMegaBytesAndKiloBytes.java
@@ -0,0 +1,17 @@
+public class CalcMegaBytesAndKiloBytes {
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(5000);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(2000);
+    }
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int iKB = kiloBytes % 1024;
+            int iMB = kiloBytes / 1024;
+            System.out.println(kiloBytes + " KB = " + iMB + " MB and " + iKB + " KB");
+        }
+    }
+}
