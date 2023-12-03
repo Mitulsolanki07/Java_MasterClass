51a874d4ee70ba5f997b971bc73b16a92732cd32
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-03 22:10:13 +0530

    We have a dog that likes to bark.  We need to wake up if the dog is barking at night!

---
+++ src/BarkingDog.java
@@ -0,0 +1,12 @@
+public class BarkingDog {
+    public static void main(String[] args) {
+        System.out.println("isNeed to wake up: " + shouldWakeUp(true, 2));
+        System.out.println("isNeed to wake up: " + shouldWakeUp(false, 5));
+        System.out.println("isNeed to wake up: " + shouldWakeUp(true, -1));
+        System.out.println("isNeed to wake up: " + shouldWakeUp(true, 8));
+    }
+
+    public static boolean shouldWakeUp(boolean isBarking, int hrOfDay) {
+        return ((hrOfDay < 8 && hrOfDay >= 0) || (hrOfDay > 22 && hrOfDay <= 23)) && isBarking;
+    }
+}
