d15e511a36f654c055de701cb4f860276e5b1e2d
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-03 21:59:47 +0530

    calculate miles per hour from km per hour , return rounded value using Math.round() : returns an int or long value that is closest to the number provided

---
+++ src/CalcMilesPerHour.java
@@ -0,0 +1,18 @@
+public class CalcMilesPerHour {
+    public static void main(String[] args) {
+        printConversion(1.5);
+        printConversion(-10.2);
+        printConversion(10.52);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+        return Math.round(kilometersPerHour / 1.609);
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+        System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+    }
+}
