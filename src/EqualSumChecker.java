775b1490791b7a203b65ef06b961f5d0445221fe
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-03 22:40:17 +0530

    it needs to return true if the sum of the first and second parameter is equal to the third parameter. Otherwise, return false.

---
+++ src/EqualSumChecker.java
@@ -0,0 +1,11 @@
+public class EqualSumChecker {
+    public static void main(String[] args) {
+        System.out.println("A + B == C :" + hasEqualSum(1, 1, 1));
+        System.out.println("A + B == C : " + hasEqualSum(1, 1, 2));
+        System.out.println("A + B == C : " + hasEqualSum(1, -1, 0));
+    }
+
+    public static boolean hasEqualSum(int numA, int numB, int numC) {
+        return (numA + numB) == numC;
+    }
+}
