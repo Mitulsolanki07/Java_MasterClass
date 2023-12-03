23823d040be643138f7b61f3357e666022d21546
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-03 21:35:53 +0530

    calculate is Leap Year problem

---
+++ src/LeapYearCalculator.java
@@ -0,0 +1,25 @@
+public class LeapYearCalculator {
+    public static void main(String[] args) {
+        System.out.println("is Leap year: " + isLeapYear(-1600));
+        System.out.println("is Leap year: " + isLeapYear(1600));
+        System.out.println("is Leap year: " + isLeapYear(2017));
+        System.out.println("is Leap year: " + isLeapYear(2000));
+
+    }
+
+    public static boolean isLeapYear(int year) {
+//        given range
+        if (year >= 1 && year <= 9999) {
+            if (year % 4 == 0) {
+//                if divisible by 100 then it must be divisible by 400
+                if (year % 100 == 0) {
+                    return year % 400 == 0;
+                }
+//                if divisible by only 4, its leap year
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+}
