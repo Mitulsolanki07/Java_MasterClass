816fc5b869843e55ea5d1b27a0bbd8107a5b2855
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-05 00:08:21 +0530

    Write a method named area with one double parameter named radius.
    The method needs to return a double value that represents the area of a circle.
    
    If the parameter radius is negative then return -1.0 to represent an invalid value.
    
    Write another overloaded method with 2 parameters x and y (both doubles), where x and y represent the sides of a rectangle.
    
    The method needs to return an area of a rectangle.

---
+++ src/AreaCalculator.java
@@ -0,0 +1,24 @@
+public class AreaCalculator {
+    public static void main(String[] args) {
+        System.out.println("Calculate area of circle: " + area(5.0));
+        System.out.println("Calculate area of circle: " + area(-1.0));
+        System.out.println("Calculate area of rectangle: " + area(5.0, 4.0));
+        System.out.println("Calculate area of rectangle: " + area(-1.0, 4.0));
+    }
+
+    public static double area(double radius) {
+//        calculate area of circle
+        if (radius < 0) {
+            return -1.0;
+        }
+        return Math.PI * radius * radius;
+    }
+
+    public static double area(double x, double y) {
+//        calculate area of rectangle
+        if (x < 0 || y < 0) {
+            return -1.0;
+        }
+        return x * y;
+    }
+}
