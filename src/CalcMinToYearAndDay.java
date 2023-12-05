f212c198d032d728d8b722817acebdc0b9b41f41
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2023-12-05 23:35:47 +0530

    If the parameter is less than 0, print text "Invalid Value".
    Otherwise, if the parameter is valid then it needs to print a message in the format "XX min = YY y and ZZ d".
    
    XX represents the original value minutes.
    YY represents the calculated years.
    ZZ represents the calculated days.

---
+++ src/CalcMinToYearAndDay.java
@@ -0,0 +1,21 @@
+import java.text.MessageFormat;
+
+public class CalcMinToYearAndDay {
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    //    calculate days and year from minutes
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            int year = (int) (minutes / 5_25_600);
+            int days = (int) (minutes / 1440) % 365;
+            System.out.println(MessageFormat.format("{0} min = {1} y and {2} d", minutes, year, days));
+        }
+
+    }
+}
