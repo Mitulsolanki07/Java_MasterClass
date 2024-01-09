a8594a0d0df93f6140f1d142a8c742fce7d92188
Mitulsolanki07 <mitulsolankiit067@gmail.com>
2024-01-09 09:24:59 +0530

    Shared Digit Write a method named hasSharedDigit with two parameters of type int.
    Each number should be within the range of 10 (inclusive) - 99 (inclusive). If one of the numbers is not within the range, the method should return false.
    
    The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false.
    
    EXAMPLE INPUT/OUTPUT:
    
    hasSharedDigit(12, 23); → should return true since the digit 2 appears in both numbers
    
    hasSharedDigit(9, 99); → should return false since 9 is not within the range of 10-99
    
    hasSharedDigit(15, 55); → should return true since the digit 5 appears in both numbers

---
+++ src/LoopExercise/sharedDigit.java
@@ -0,0 +1,25 @@
+package LoopExercise;
+
+public class sharedDigit {
+    public static void main(String[] args) {
+        int a = 12, b = 23;
+        System.out.println("hasSharedDigits for number a: " + a + " , b: " + b + " = " + hasSharedDigit(a, b));
+        a = 9;
+        b = 99;
+        System.out.println("hasSharedDigits for number a: " + a + " , b: " + b + " = " + hasSharedDigit(a, b));
+        a = 15;
+        b = 55;
+        System.out.println("hasSharedDigits for number a: " + a + " , b: " + b + " = " + hasSharedDigit(a, b));
+
+    }
+
+    public static boolean hasSharedDigit(int number1, int number2) {
+        if ((number1 < 10 | number1 > 99) | (number2 < 10 | number2 > 99)) {
+            System.out.println("should return false since a or b " + number1 + ", " + number2 + " is not within the range of 10-99");
+            return false;
+        } else {
+
+            return (number1 % 10 == number2 / 10 | number1 / 10 == number2 / 10 | number1 / 10 == number2 % 10 | number1 % 10 == number2 % 10);
+        }
+    }
+}
